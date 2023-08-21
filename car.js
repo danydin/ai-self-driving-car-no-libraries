aced30a95b40e4e9872ce636df34c9a8707ebb99
dean ben <19169228+danydin@users.noreply.github.com>
2023-08-22 00:18:25 +0800

    updated car keyboard all directions include physics of gravity sin,cos aka unit circle

--- car.js
+++ car.js
@@ -11,15 +11,20 @@ class Car {
         this.acceleration = 0.2;
         this.maxSpeed = 3;
         this.friction = 0.05;
+        this.angle = 0;
 
         // reference to a a control object
         this.controller = new CarController();
     }
 
     updateCarController() {
+        // create another private method to make code cleaner
+        this.#move()
+    }
+
+    #move() {
         if(this.controller.up) {
             this.speed += this.acceleration;
-            // console.log(this.y);
         }
         if(this.controller.down) {
             this.speed -= this.acceleration;
@@ -36,27 +41,41 @@ class Car {
         if(this.speed<0) {
             this.speed += this.friction;
         }
+        // if not using this formula the car will keep moving in tiny increments indefinitely
         if(Math.abs(this.speed)<this.friction) {
             this.speed = 0;
         }
-        if(this.controller.left){
-            this.x -= 2;
+
+        if(this.speed!=0){
+            const flip = this.speed>0?1:-1;
+            if(this.controller.left){
+                this.angle += 0.03*flip;
+                // old method: this.x -= 2;
+            }
+            if(this.controller.right){
+                this.angle -= 0.03*flip;
+                // old method: this.x += 2;
+            }
         }
-        if(this.controller.right){
-            this.
-        }
-        this.y -= this.speed;
+
+        this.x -= Math.sin(this.angle)*this.speed;
+        this.y -= Math.cos(this.angle)*this.speed;
+        // old method: this.y -= this.speed;
     }
 
     // method to draw the car on canvas
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle)
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            - this.height / 2,
             this.width,
             this.height
         );
         ctx.fill();
+        ctx.restore();
     }
 }
