25f43887a90b7261250967bcb81d313f5465ffd9
dean ben <19169228+danydin@users.noreply.github.com>
2023-09-01 18:20:38 +0800

    started working on the ai brain of the automated car

---
+++ aineurons.js
@@ -0,0 +1,15 @@
+class Level() {
+    constructor(inputsNodes, outputNodes) {
+        this.inputs = new Array(inputsNodes); 
+        this.outputs = new Array(outputNodes);
+        this.biases = new Array(outputNodes); // the number here for each output will decide if it will 'fire' or not
+
+        this.connectionsPerNodes = []; // amount of connections/neurons from each input to each output 
+        for (let i=0 ; i<inputHeads; i++) {
+            this.connectionsPerNodes[i] = new Array(outputNodes); // set the amounts of connections to the amounts of output heads basically for each output head we set a connection
+        }
+        
+        Level.#randomize(); // set the biases to random values
+    }
+}
+
