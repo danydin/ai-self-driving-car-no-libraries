3a26ad2dd92048dbc9fee2413d610724b1b6a014
dean ben <19169228+danydin@users.noreply.github.com>
2023-09-03 00:02:18 +0800

    added randomize static method

--- aineurons.js
+++ aineurons.js
@@ -12,7 +12,7 @@ class Layer {
         Level.#randomize(); // set the biases to random values
     }
 
-    // static hecause we need to seralize it
+    // static hecause we need to seralize 
     static #randomize(layer) {
 
     }
